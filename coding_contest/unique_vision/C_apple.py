270a2af0f5b3fa8a087e911be6ad7b3d1a1d9dc8
katsu-rai <taradancan0603@gmail.com>
2023-07-31 16:46:41 -0600

    2023/07/31

--- coding_contest/unique_vision/C_apple.py
+++ coding_contest/unique_vision/C_apple.py
@@ -1,13 +1,17 @@
-n, m = map(int, input().split())
-sellers = []
-buyers = []
-x = 10000000000
+# n, m = map(int, input().split())
+# sellers = []
+# buyers = []
+# x = 10000000000
 
 
-sellers = list(map(int, input().split()))
-buyers = list(map(int, input().split()))
+# sellers = list(map(int, input().split()))
+# buyers = list(map(int, input().split()))
 
-for i in sellers:
-    for j in buyers:
-        if j >= i:
-            x = max(x, i)
+# for i in sellers:
+#     for j in buyers:
+#         if j >= i:
+#             x = max(x, i)
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+
+print(list1 + list2)
