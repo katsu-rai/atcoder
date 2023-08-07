741406edcdb1a5288b4c93136f8c73302febf62b
katsu-rai <taradancan0603@gmail.com>
2023-08-07 10:34:21 -0600

    2023/08/07

---
+++ practice_test/C_daydream.py
@@ -0,0 +1,9 @@
+S = input()
+
+# dream
+# dreamer
+# erase
+# eraser
+
+# while True:
+#     if S[0] == "d"
--- practice_test/practice.py
+++ practice_test/practice.py
@@ -1,14 +1,29 @@
-S = input()
+n = int(input())
+list = list(map(int, input().split()))
 
-if (
-    S == "ACE"
-    or S == "BDF"
-    or S == "CEG"
-    or S == "DFA"
-    or S == "EGB"
-    or S == "FAC"
-    or S == "GBD"
-):
-    print("Yes")
-else:
-    print("No")
+# s1 = list[0]
+
+# list.sort(reverse=True)
+# stronggest = list[0]
+
+# if s1 == stronggest and s1 == list[1]:
+#     print(1)
+# elif s1 == stronggest:
+#     print(0)
+# else:
+#     print(stronggest - s1 + 1)
+
+s1 = list[0]
+
+list.remove(list[0])
+ans = 0
+
+for i in list:
+    if s1 < i:
+        s1 += i - s1 + 1
+        ans += i - s1 + 1
+    elif s1 == i:
+        s1 += 1
+        ans += 1
+
+print(ans)
